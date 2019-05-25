ee3e6843a1cc2692d145379ac959eb5adaa8577f
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-05-25 02:00:50 -0500

    Added class StaticHashing

---
+++ include/statichashing.h
@@ -0,0 +1,14 @@
+#ifndef STATIC_HASHING_H_
+#define STATIC_HASHING_H_
+
+#include "register.h"
+#include "organizer.h"
+
+class StaticHashing : public Organizer {
+// TODO : Define variables and methods
+
+public:
+	StaticHashing(std::string filename);
+};
+
+#endif
