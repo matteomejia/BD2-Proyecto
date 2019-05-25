689a7569486699763af7d8235d85b3d6566b6c1b
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-05-25 02:01:51 -0500

    Added class BPlusTreeIndex

---
+++ include/bplustreeindex.h
@@ -0,0 +1,14 @@
+#ifndef B_PLUS_TREE_INDEX_H_
+#define B_PLUS_TREE_INDEX_H_
+
+#include "register.h"
+#include "organizer.h"
+
+class BPlusTreeIndex : public Organizer {
+// TODO : Define variables and methods
+
+public:
+	BPlusTreeIndex(std::string filename);
+};
+
+#endif
