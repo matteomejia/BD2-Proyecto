5352c0650831e12b8ba4d869306e91b9a0785282
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-05-25 01:59:18 -0500

    Added class Register

---
+++ include/register.h
@@ -0,0 +1,17 @@
+#ifndef REGISTER_H_
+#define REGISTER_H_
+
+class Register {
+	int id_;
+	// TODO : Define all attributes...
+
+public:
+	Register();
+	Register(
+		int id
+		// TODO : Add attributes
+	);
+	Register(const Register& record); // Copy constructor
+};
+
+#endif
---
+++ lib/register.cpp
@@ -0,0 +1,17 @@
+#include "register.h"
+
+Register::Register() {
+	// TODO : Initialize empty register
+}
+
+Register::Register(
+	int id
+	// TODO : Add attributes
+) :
+	id_(id)
+	// Add remaining attributes to initializer list
+{}
+
+Register::Register(const Register& record) {
+	// TODO : Copy data from record to *this
+}
