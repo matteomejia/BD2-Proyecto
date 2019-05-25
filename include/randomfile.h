d57d4bec6bbadd09317538dcbe279d0ccd83c02f
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-05-25 02:00:11 -0500

    Added class RandomFile

---
+++ include/randomfile.h
@@ -0,0 +1,14 @@
+#ifndef RANDOM_FILE_H_
+#define RANDOM_FILE_H_
+
+#include "register.h"
+#include "organizer.h"
+
+class RandomFile : public Organizer {
+// TODO : Define variables and methods
+
+public:
+	RandomFile(std::string filename);
+};
+
+#endif
---
+++ lib/randomfile.cpp
@@ -0,0 +1,24 @@
+#include "record.h"
+#include "randomfile.h"
+
+RandomFile::RandomFile(std::string filename) {
+	this->filename_ = filename;
+
+	// TODO : Initialize random file
+}
+
+void RandomFile::writeRecord(Record record) {
+}
+	// TODO : Open this->filename and write raw record
+
+void RandomFile::readRecord(int index) {
+	// TODO : Open this->filename and read raw record in position index
+}
+
+void RandomFile::insert(Register record) {
+	// TODO : Insert record into table
+}
+
+Register RandomFile::search(int record_id) {
+	// TODO : Find and return register with id = record_id
+}
