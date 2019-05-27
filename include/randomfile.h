c27439be0a746285bc15de2f369687ec5b9a9151
Francisco <matteomejia@github.com>
2019-05-26 19:26:13 -0500

    Random File almost

--- include/randomfile.h
+++ include/randomfile.h
@@ -2,19 +2,59 @@
 #define RANDOM_FILE_H_
 
 #include <iostream>
+#include<fstream>
+#include<cstdio>
 
-#include "register.h"
-#include "fileorganizer.h"
+using namespace std;
 
-class RandomFile : public FileOrganizer
-{
-	// TODO : Define variables and methods
-	std::vector<std::pair<int, std::string>> pairs;
+void writeString(fstream &stream, string str);
+
+string readString(fstream &stream);
+
+class Record {
+private:
+    string name;
+    string age;
+    string nationality;
+    string overall;
+    string club;
+    string value;
+    string position;
+    string jersey_number;
+
 public:
-	RandomFile(std::string filename, std::string outfile);
+    void setData();
 
-	void insert(Register record) override;
-	Register search(int record_id) override;
+    void showData();
+
+    long write(fstream &stream);
+
+    bool read(fstream &stream);
 };
+
+class VariableRecordFile {
+
+private:
+    string fileName;
+    string indexName;
+
+public:
+    VariableRecordFile(string &_fileName);
+
+    void writeRecord(Record obj);
+
+    void scanAll();
+
+/*
+* function to read a record
+*/
+    Record readRecord(int n);
+
+    int size();
+};
+
+void writeString(fstream &stream, string str);
+
+string readString(fstream &stream);
 
 #endif
